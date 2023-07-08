3cc65e32c8b2eeba2ee031523964f7e207151099
Latanshu Varshney <95296749+Latanshu14@users.noreply.github.com>
2023-07-08 13:43:07 +0530

    Update bot.py

--- frontend/bot.py
+++ frontend/bot.py
@@ -4,7 +4,7 @@ import json
 from discord.ext import commands
 
 def run_discord_bot():
-    TOKEN = 'MTEyNTQwODgzNzQ5NTY5NzQ3OA.Gnz-jn.ToZiULAY7zDKPBI6ifv8Zg49CJauoq-20AyUVA'
+    
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
@@ -16,6 +16,10 @@ def run_discord_bot():
         print(f'{client.user} is ready.')
 
     @client.command()
+    async def hi(ctx):
+        await ctx.send("Hey there! To get started use '-helper' command.")
+        
+    @client.command()
     async def helper(ctx):
         embed = discord.Embed(title="Contest Seeker", description="Bot Command line", color=0x00FFFF)
         commandArray = list()
@@ -24,16 +28,28 @@ def run_discord_bot():
         funcArray.append("To get leaderboard")
         funcArray.append("To add an userID in leaderboard")
         funcArray.append("To delete an userID in leaderboard")
+        funcArray.append("To see upcoming codeforces contests")
+        funcArray.append("To see upcoming codechef contests")
+        funcArray.append("To see upcoming atcoder contests")
         commandArray.append("-leaderboard create")
         commandArray.append("-leaderboard show <LEADERBOARD_ID>")
         commandArray.append("-leaderboard add <LEADERBOARD_ID> <USER_ID>")
         commandArray.append("-leaderboard delete <LEADERBOARD_ID> <USER_ID>")
+        commandArray.append("-contests cf")
+        commandArray.append("-contests cc")
+        commandArray.append("-contests ac")
         l = len(commandArray)
         for i in range(l):
             embed.add_field(
                 name=f'***#{i+1}***', value=f'*{funcArray[i]}*  ➤ `{commandArray[i]}`', inline=False)
         await ctx.send(embed=embed)
 
+    @client.command()
+    async def contests(ctx, *args):
+        tempData = requests.get(f'https://contest-seeker.onrender.com/api/contests/{args[0]}')
+        tempData = tempData.json()
+        embed = showUpcoming(args[0], tempData)
+        await ctx.send(embed=embed)
     @client.command()
     async def leaderboard(ctx, *args):
         if args[0] == "create":
@@ -88,4 +104,16 @@ def show(id):
     for i in range(l):
         embed.add_field(
             name=f'***Rank #{i+1}***', value=f'*{memberList[i][1]}*  ➤ `{memberList[i][0]}` ➤ `{memberList[i][2]}`', inline=False)
+    return embed
+
+def showUpcoming(id, data):
+    embed = discord.Embed(title="Upcoming " + id + " contest: ", color=0x00FFFF)
+    contestListArray = list()
+    for element in data:
+        contestListArray.append(element["name"])
+            
+    l = len(contestListArray)
+    for i in range(l):
+        embed.add_field(
+            name=f'***#{i+1}***', value=f'*{contestListArray[i]}*', inline=False)
     return embed
